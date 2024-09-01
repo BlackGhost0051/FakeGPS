2e2681a5215cc9cdc051e1a2b96fcbc82a321d5a
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-01 18:33:06 +0200

    Add menu

--- app/src/main/java/com/blackghost/fakegps/MainActivity.java
+++ app/src/main/java/com/blackghost/fakegps/MainActivity.java
@@ -1,22 +1,65 @@
 package com.blackghost.fakegps;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.blackghost.fakegps.Fragments.MapFragment;
+import com.blackghost.fakegps.Fragments.SettingsFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.black));
+
         fragmentR(new MapFragment());
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.map) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    toolbar.setTitle("Map");
+                    fragmentR(new MapFragment());
+                    return true;
+                }
+                if (item.getItemId() == R.id.settings) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    toolbar.setTitle("Settings");
+                    fragmentR(new SettingsFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 
     private void fragmentR(Fragment fragment) {
