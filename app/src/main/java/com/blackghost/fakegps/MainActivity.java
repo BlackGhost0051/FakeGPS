7d12a632f35cb9cc231cb08964be47cac55ef39e
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-01 10:39:09 +0200

    Add fragment

--- app/src/main/java/com/blackghost/fakegps/MainActivity.java
+++ app/src/main/java/com/blackghost/fakegps/MainActivity.java
@@ -3,6 +3,11 @@ package com.blackghost.fakegps;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.blackghost.fakegps.Fragments.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentR(new MapFragment());
+    }
+
+    private void fragmentR(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
