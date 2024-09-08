289860102c6b4406d022340df5c9808d7ae1f8ed
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-08 19:38:48 +0200

    Add test HTML

--- app/src/main/java/com/blackghost/fakegps/Fragments/InfoFragment.java
+++ app/src/main/java/com/blackghost/fakegps/Fragments/InfoFragment.java
@@ -7,12 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.blackghost.fakegps.R;
 
 
 public class InfoFragment extends Fragment {
 
+    private WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,15 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
+
+        webView = view.findViewById(R.id.webView);
+
+        // Enable JS
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/info.html");
 
         return view;
     }
