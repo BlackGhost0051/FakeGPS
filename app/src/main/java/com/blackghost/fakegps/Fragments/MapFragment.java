2b2bcfa27e7cbaefc3296c0fb2ec287585bca443
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-13 19:28:51 +0200

    Test myLocationListener

--- app/src/main/java/com/blackghost/fakegps/Fragments/MapFragment.java
+++ app/src/main/java/com/blackghost/fakegps/Fragments/MapFragment.java
@@ -4,12 +4,15 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +29,7 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
@@ -42,6 +46,8 @@ public class MapFragment extends Fragment implements MainActivityInterface {
     private FakeGPSManager fakeGPSManager;
 
     private MainActivityInterface activityInterface;
+
+    private Marker myLocationMarker;
 
 
     private ImageButton myLocationBtn;
@@ -84,15 +90,7 @@ public class MapFragment extends Fragment implements MainActivityInterface {
 
         loadMap();
 
-        activityInterface.setLocation();
-
-
-        //fakeGPSManager.setLocation(36.1699, -115.1398, 5.0f, 2500); // Las Vegas
-
-
-
-
-
+        setWaypointPath();
 
         myLocationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,14 +102,54 @@ public class MapFragment extends Fragment implements MainActivityInterface {
                 }
 
                 LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
+                LocationListener locationListener = new LocationListener() {
+
+                    @Override
+                    public void onLocationChanged(@NonNull Location location) {
+
+                        Log.d("onLocationChanged", "Start");
+                        GeoPoint currentLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
+
+                        if (myLocationMarker != null) {
+                            mapView.getOverlays().remove(myLocationMarker);
+                        }
+
+                        myLocationMarker = new Marker(mapView);
+                        myLocationMarker.setPosition(currentLocation);
+                        myLocationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+
+                        mapView.getOverlays().add(myLocationMarker);
+                        mapView.invalidate();
+
+                        mapController.setCenter(currentLocation);
+                        mapController.setZoom(20.0f);
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+                    @Override
+                    public void onProviderEnabled(String provider) { }
+
+                    @Override
+                    public void onProviderDisabled(String provider) { }
+                };
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 4000, 10, locationListener);
+
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 if (lastKnownLocation != null) {
                     GeoPoint currentLocation = new GeoPoint(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
                     mapView.getController().setCenter(currentLocation);
                     mapController.setZoom(15.0);
 
-                    Toast.makeText(getContext(), "Centered on current location", Toast.LENGTH_SHORT).show();
+                    myLocationMarker = new Marker(mapView);
+                    myLocationMarker.setPosition(currentLocation);
+                    myLocationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+
+                    mapView.getOverlays().add(myLocationMarker);
+                    mapView.invalidate();
                 } else {
                     Toast.makeText(getContext(), "Unable to retrieve current location", Toast.LENGTH_SHORT).show();
                 }
@@ -144,20 +182,20 @@ public class MapFragment extends Fragment implements MainActivityInterface {
         List<Location> waypoints = new ArrayList<>();
 
         Location point1 = new Location(LocationManager.GPS_PROVIDER);
-        point1.setLatitude(37.7749); // San Francisco
+        point1.setLatitude(37.7749);
         point1.setLongitude(-122.4194);
         point1.setAccuracy(5.0f);
         waypoints.add(point1);
 
         Location point2 = new Location(LocationManager.GPS_PROVIDER);
-        point2.setLatitude(34.0522); // Los Angeles
-        point2.setLongitude(-118.2437);
+        point2.setLatitude(36.8749);
+        point2.setLongitude(-122.4194);
         point2.setAccuracy(5.0f);
         waypoints.add(point2);
 
         Location point3 = new Location(LocationManager.GPS_PROVIDER);
-        point3.setLatitude(36.1699); // Las Vegas
-        point3.setLongitude(-115.1398);
+        point3.setLatitude(35.9749);
+        point3.setLongitude(-122.4194);
         point3.setAccuracy(5.0f);
         waypoints.add(point3);
 
