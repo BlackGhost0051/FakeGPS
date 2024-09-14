9dedac1fb0e89a662fef8925cfd13126d25e1bde
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-09-14 18:07:16 +0200

    Mod preferences

--- app/src/main/java/com/blackghost/fakegps/Fragments/SettingsFragment.java
+++ app/src/main/java/com/blackghost/fakegps/Fragments/SettingsFragment.java
@@ -12,4 +12,10 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getContext().getTheme().applyStyle(R.style.preferences_style, true);
+    }
 }
