53c8b38dcfb29084797ead3e9d87c6bb979c1b80
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2026-02-16 14:27:18 +0100

    Add dark theme logic in Settings Fragment

--- app/src/main/java/com/blackghost/fakegps/Fragments/SettingsFragment.java
+++ app/src/main/java/com/blackghost/fakegps/Fragments/SettingsFragment.java
@@ -3,11 +3,14 @@ package com.blackghost.fakegps.Fragments;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
 import com.blackghost.fakegps.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
@@ -17,5 +20,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getContext().getTheme().applyStyle(R.style.preferences_style, true);
+
+        SwitchPreference darkThemePref = findPreference("dark_theme");
+        if (darkThemePref != null) {
+            darkThemePref.setOnPreferenceChangeListener((preference, newValue) -> {
+                boolean isDark = (boolean) newValue;
+                AppCompatDelegate.setDefaultNightMode(
+                        isDark ? AppCompatDelegate.MODE_NIGHT_YES
+                               : AppCompatDelegate.MODE_NIGHT_NO
+                );
+                return true;
+            });
+        }
     }
 }
